913dad8d3bdd39bd60040f1c8f4e3ef1f9551ef7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-05 17:22:21 +0100

    disable firewall

--- hosts/x260/configuration.nix
+++ hosts/x260/configuration.nix
@@ -174,7 +174,7 @@
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
