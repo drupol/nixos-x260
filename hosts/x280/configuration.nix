8e5d5dc1db476d304cc0a7a6f40d1d5e3b915acd
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-19 23:26:47 +0200

    Add Thinkpad x280.

--- hosts/x280/configuration.nix
+++ hosts/x280/configuration.nix
@@ -3,20 +3,14 @@
 {
   imports =
     [
+      nixos-hardware.nixosModules.lenovo-thinkpad-x280
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
-  # boot.loader.grub.useOSProber = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "nodev"; # or "nodev" for efi only
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
