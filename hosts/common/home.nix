c6cdc9a0ffa268d7b9512c248730d2aab83babee
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-27 15:11:51 +0200

    Fix path to file.

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -29,7 +29,7 @@
       recursive = true;
     };
     # Credits to https://store.kde.org/p/1272202
-    "${config.homeDirectory}/Pictures/Backgrounds/Starry Nebula 219.png" = {
+    "Pictures/Backgrounds/Starry Nebula 219.png" = {
       source = ./. + "/../../files/home/pol/Pictures/Backgrounds/Starry Nebula 219.png";
       recursive = true;
     };
