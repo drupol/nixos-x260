64b6ce275fc3fa1ebc4abc7c65c4a451c105071e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-30 07:25:31 +0200

    refactor: more abstraction !

---
+++ hosts.nix
@@ -0,0 +1,58 @@
+[
+  {
+    instance = "ec2";
+    hostname = "ec2";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+  {
+    instance = "elitebook820";
+    hostname = "elitebook820";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+  {
+    instance = "lw196205087";
+    hostname = "lw196205087";
+    operating-system = "ubuntu";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "devlin";
+  }
+  {
+    instance = "nixos";
+    hostname = "nixos";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+  {
+    instance = "x13";
+    hostname = "x13";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+  {
+    instance = "x260";
+    hostname = "x260";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+  {
+    instance = "x280";
+    hostname = "x280";
+    operating-system = "nixos";
+    system = "x86_64-linux";
+    sshIdentityFile = "private.pem";
+    user = "pol";
+  }
+]
