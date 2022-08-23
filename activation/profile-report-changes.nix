4d22782e5e50f11705df357c34197e1af6e82676
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-08-23 09:57:59 +0200

    Activation update: clone some projects.

--- activation/profile-report-changes.nix
+++ activation/profile-report-changes.nix
@@ -1,7 +1,28 @@
 { config, pkgs, lib, ... }:
 
+let
+  gitClone = { source, path }: lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    if [[ ! -d "${path}" ]]; then
+      ${pkgs.git}/bin/git clone ${source} ${path}
+    fi
+
+    if [ -z "$(${pkgs.git}/bin/git --git-dir=${path}/.git --work-tree=${path} status --porcelain)" ]; then
+      ${pkgs.git}/bin/git --git-dir=${path}/.git --work-tree=${path} pull
+    fi
+  '';
+in
 {
-  home.activation.profile-report-changes = ''
+  home.activation.initPasswordStore = gitClone {
+    source = "git@github.com:drupol/pass.git";
+    path = "${config.xdg.configHome}/.password-store/";
+  };
+
+  home.activation.initWebsite = gitClone {
+    source = "git@github.com:drupol/not-a-number.io.git";
+    path = "${config.home.homeDirectory}/Code/drupol/not-a-number.io/";
+  };
+
+  home.activation.profile-report-changes = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
     nvd diff $(ls -dv /nix/var/nix/profiles/per-user/${config.home.username}/home-manager-*-link | tail -2)
   '';
