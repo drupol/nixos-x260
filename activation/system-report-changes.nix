5596cd301847097ce1a9e0d3036a5444afe11c0f
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-09-26 22:54:15 +0200

    Update activation scripts

--- activation/system-report-changes.nix
+++ activation/system-report-changes.nix
@@ -5,11 +5,11 @@
     PATH=$PATH:${lib.makeBinPath [ pkgs.nvd pkgs.nix ]}
 
     # Disable nvd if there are lesser than 2 profiles in the system.
-    if [ `ls -d1v /nix/var/nix/profiles/system-*-link 2>/dev/null | wc -l ` -lt 2 ];
+    if [ $(ls -d1v /nix/var/nix/profiles/system-*-link 2>/dev/null | wc -l) -lt 2 ];
     then
-        return 0
+        echo "Skipping reporting changes..."
+    else
+        nvd diff $(ls -d1v /nix/var/nix/profiles/system-*-link | tail -2)
     fi
-
-    nvd diff $(ls -dv /nix/var/nix/profiles/system-*-link | tail -2)
   '';
 }
