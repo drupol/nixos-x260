d26256938d033f11529699f27fee7551f647bc28
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-12-05 17:09:49 +0100

    Update Nix config

--- hosts/common/config.nix
+++ hosts/common/config.nix
@@ -11,7 +11,14 @@
       trusted-users = [ "root" "pol" ];
       auto-optimise-store = true;
     };
+    # From https://jackson.dev/post/nix-reasonable-defaults/
     extraOptions = ''
+      connect-timeout = 5
+      log-lines = 25
+      min-free = 128000000
+      max-free = 1000000000
+      fallback = true
+      warn-dirty = false
       keep-outputs = true
       keep-derivations = true
       experimental-features = nix-command flakes
