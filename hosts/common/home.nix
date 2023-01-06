7646c1d32e99ec9ce840e4dfe0f22e0e817f7351
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-06 10:45:54 +0100

    Remove `https-everywhere` firefox addon

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -54,7 +54,6 @@
         pkgs.nur.repos.rycee.firefox-addons.browserpass
         pkgs.nur.repos.rycee.firefox-addons.enhancer-for-youtube
         pkgs.nur.repos.rycee.firefox-addons.french-dictionary
-        pkgs.nur.repos.rycee.firefox-addons.https-everywhere
         pkgs.nur.repos.rycee.firefox-addons.i-dont-care-about-cookies
         pkgs.nur.repos.rycee.firefox-addons.kristofferhagen-nord-theme
         pkgs.nur.repos.rycee.firefox-addons.multi-account-containers
