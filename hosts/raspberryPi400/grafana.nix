cb151e5eb56a30496baf4eed9cdbb4576640a4df
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-18 15:31:34 +0100

    update `grafana`

--- hosts/raspberryPi400/grafana.nix
+++ hosts/raspberryPi400/grafana.nix
@@ -13,14 +13,12 @@
       {
         name = "Prometheus";
         type = "prometheus";
-        access = "proxy";
         url = "http://localhost:9001";
         isDefault = true;
       }
       {
         name = "Loki";
         type = "loki";
-        access = "proxy";
         url = "http://localhost:3100";
         isDefault = false;
       }
