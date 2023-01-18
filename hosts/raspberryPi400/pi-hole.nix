df78c0b63324943b3af0776ccc6e022c5c102754
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-18 13:56:34 +0100

    pi hole update

--- hosts/raspberryPi400/pi-hole.nix
+++ hosts/raspberryPi400/pi-hole.nix
@@ -19,8 +19,8 @@
     DNSMASQ_USER = "root";
     CUSTOM_CACHE_SIZE = "10000";
     DHCP_ACTIVE = "true";
-    DHCP_IPv6 = "false";
-    IPv6 = "false";
+    DHCP_IPv6 = "true";
+    IPv6 = "true";
     DHCP_START = "192.168.2.100";
     DHCP_END = "192.168.2.200";
     DHCP_ROUTER = "192.168.2.10";
@@ -28,6 +28,10 @@
     DHCP_rapid_commit = "true";
     DNS_BOGUS_PRIV = "true";
     DNS_FQDN_REQUIRED = "true";
+    TEMPERATUREUNIT = "c";
+    WEBUIBOXEDLAYOUT = "traditional";
+    QUERY_LOGGING = "false";
+    WEBTHEME = "default-dark";
   };
   volumes = [
     "pihole:/etc/pihole"
