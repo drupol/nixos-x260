37d405170c3d5f0926116b958ee52e1a3b9c1399
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-18 09:20:37 +0100

    update network

--- hosts/raspberryPi400/configuration.nix
+++ hosts/raspberryPi400/configuration.nix
@@ -93,14 +93,14 @@
     nat = {
       enable = true;
       internalInterfaces = [ "end0" ];
-      externalInterface = "enp1s0u1u2";
+      externalInterface = "enp1s0u2";
     };
 
     interfaces.end0.ipv4.addresses = [{
       address = "192.168.2.10";
       prefixLength = 24;
     }];
-    interfaces.enp1s0u1u2.ipv4.addresses = [{
+    interfaces.enp1s0u2.ipv4.addresses = [{
       address = "192.168.1.2";
       prefixLength = 24;
     }];
