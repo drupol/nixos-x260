719c47f976b0ec6fa5c55841c3581c290653999b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-18 10:13:14 +0100

    typos

--- hosts/raspberryPi400/pi-hole.nix
+++ hosts/raspberryPi400/pi-hole.nix
@@ -10,23 +10,23 @@
     TZ = "Europe/Brussels";
     WEBPASSWORD = "webpassword";
     DNSMASQ_LISTENING = "all";
-    FTLCONF_REPLY_ADDR4: "192.168.2.10"
-    ServerIP: "192.168.2.10"
-    VIRTUAL_HOST:  "pihole.dns"
-    DNSMASQ_LISTENING: "all"
-    PIHOLE_DOMAIN: "local"
-    DNSMASQ_USER: "root"
-    CUSTOM_CACHE_SIZE: "10000"
-    DHCP_ACTIVE: "true"
-    DHCP_IPv6: "false"
-    IPv6: "false"
-    DHCP_START: "192.168.2.100"
-    DHCP_END: "192.168.2.200"
-    DHCP_ROUTER: "192.168.2.10"
-    DHCP_LEASETIME: "24"
-    DHCP_rapid_commit: "true"
-    DNS_BOGUS_PRIV: "true"
-    DNS_FQDN_REQUIRED: "true"
+    FTLCONF_REPLY_ADDR4: "192.168.2.10";
+    ServerIP = "192.168.2.10";
+    VIRTUAL_HOST = "pihole.dns";
+    DNSMASQ_LISTENING = "all";
+    PIHOLE_DOMAIN = "local";
+    DNSMASQ_USER = "root";
+    CUSTOM_CACHE_SIZE = "10000";
+    DHCP_ACTIVE = "true";
+    DHCP_IPv6 = "false";
+    IPv6 = "false";
+    DHCP_START = "192.168.2.100";
+    DHCP_END ="192.168.2.200";
+    DHCP_ROUTER = "192.168.2.10";
+    DHCP_LEASETIME = "24";
+    DHCP_rapid_commit = "true";
+    DNS_BOGUS_PRIV = "true";
+    DNS_FQDN_REQUIRED = "true";
   };
   volumes = [
     "pihole:/etc/pihole"
