fe5bd0eaca013db5a44d10f1e96ad25a423e0372
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-18 09:09:26 +0100

    switch to network scripting files temporarily

--- hosts/raspberryPi400/configuration.nix
+++ hosts/raspberryPi400/configuration.nix
@@ -97,7 +97,7 @@
     };
 
     interfaces.end0.ipv4.addresses = [{
-      address = "192.168.2.11";
+      address = "192.168.2.10";
       prefixLength = 24;
     }];
     interfaces.enp1s0u1u2.ipv4.addresses = [{
