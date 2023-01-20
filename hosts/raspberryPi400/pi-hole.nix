02be1c540953d14d0c4844755c3192fd1d855dfc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-01-20 11:27:12 +0100

    update `pihole` config

--- hosts/raspberryPi400/pi-hole.nix
+++ hosts/raspberryPi400/pi-hole.nix
@@ -31,7 +31,23 @@
     WEBUIBOXEDLAYOUT = "traditional";
     QUERY_LOGGING = "false";
     FTLCONF_REPLY_ADDR4 = "192.168.2.10";
+    FTLCONF_DBFILE = "";
     WEBTHEME = "default-dark";
+    PIHOLE_DNS_1 = "8.8.8.8";
+    PIHOLE_DNS_2 = "8.8.4.4";
+    PIHOLE_DNS_3 = "2001:4860:4860:0:0:0:0:8888";
+    PIHOLE_DNS_4 = "2001:4860:4860:0:0:0:0:8844";
+    PIHOLE_DNS_5 = " 208.67.222.222";
+    PIHOLE_DNS_6 = "208.67.220.220";
+    PIHOLE_DNS_7 = "2620:119:35::35";
+    PIHOLE_DNS_8 = "2620:119:53::53";
+    PIHOLE_DNS_9 = "1.1.1.1";
+    PIHOLE_DNS_10 = "1.0.0.1";
+    PIHOLE_DNS_11 = "2606:4700:4700::1111";
+    PIHOLE_DNS_12 = "2606:4700:4700::1001";
+    BLOCKING_ENABLED = "true";
+    IPV4_ADDRESS = "0.0.0.0";
+    IPV6_ADDRESS = "0:0:0:0:0:0";
   };
   volumes = [
     "pihole:/etc/pihole"
