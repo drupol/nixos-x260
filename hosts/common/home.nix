d43a2f02e40ef88c66a30e88a784e2e8034adae5
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-05 16:17:11 +0100

    add `pkgs.vscode-extensions.mkhl.direnv` vscode extension

--- hosts/common/home.nix
+++ hosts/common/home.nix
@@ -461,6 +461,7 @@
         pkgs.vscode-extensions.jnoortheen.nix-ide
         pkgs.vscode-extensions.matklad.rust-analyzer
         pkgs.vscode-extensions.mhutchie.git-graph
+        pkgs.vscode-extensions.mkhl.direnv
         pkgs.vscode-extensions.ms-vscode.makefile-tools
         pkgs.vscode-extensions.ms-vscode-remote.remote-ssh
         pkgs.vscode-extensions.pkief.material-icon-theme
