fb3ccf9567cd14e81a10883fd0df69b1fab1d519
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-07 09:12:09 +0100

    raspberry: disable `ipv6`

--- hosts/raspberryPi400/pi-hole.nix
+++ hosts/raspberryPi400/pi-hole.nix
@@ -46,9 +46,9 @@
           PIHOLE_DOMAIN = "local";
           DNSMASQ_USER = "root";
           CUSTOM_CACHE_SIZE = "10000";
-          IPv6 = "true";
+          IPv6 = "false";
           DHCP_ACTIVE = "true";
-          DHCP_IPv6 = "true";
+          DHCP_IPv6 = "false";
           DHCP_START = "192.168.2.100";
           DHCP_END = "192.168.2.200";
           DHCP_ROUTER = "192.168.2.10";
@@ -63,19 +63,12 @@
           WEBTHEME = "default-dark";
           PIHOLE_DNS_1 = "8.8.8.8";
           PIHOLE_DNS_2 = "8.8.4.4";
-          PIHOLE_DNS_3 = "2001:4860:4860:0:0:0:0:8888";
-          PIHOLE_DNS_4 = "2001:4860:4860:0:0:0:0:8844";
           PIHOLE_DNS_5 = " 208.67.222.222";
           PIHOLE_DNS_6 = "208.67.220.220";
-          PIHOLE_DNS_7 = "2620:119:35::35";
-          PIHOLE_DNS_8 = "2620:119:53::53";
           PIHOLE_DNS_9 = "1.1.1.1";
           PIHOLE_DNS_10 = "1.0.0.1";
-          PIHOLE_DNS_11 = "2606:4700:4700::1111";
-          PIHOLE_DNS_12 = "2606:4700:4700::1001";
           BLOCKING_ENABLED = "true";
           IPV4_ADDRESS = "0.0.0.0";
-          IPV6_ADDRESS = "0:0:0:0:0:0";
         };
         volumes = [
           "pihole:/etc/pihole"
