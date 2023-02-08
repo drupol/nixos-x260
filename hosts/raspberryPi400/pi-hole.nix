1ab25e662b6470277236947b31f9e5a7609f0c10
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-08 08:24:59 +0100

    switch from podman to docker

--- hosts/raspberryPi400/pi-hole.nix
+++ hosts/raspberryPi400/pi-hole.nix
@@ -5,27 +5,17 @@
 }: {
   networking = {
     firewall = {
-      allowedTCPPorts = [53 67 80 8888];
-      allowedUDPPorts = [53 67];
+      allowedTCPPorts = [53 67 80];
+      allowedUDPPorts = [53 67 547];
     };
   };
 
   virtualisation = {
     containers = {
       enable = true;
-      containersConf = {
-        settings = {
-          engine.helper_binaries_dir = ["${pkgs.netavark}/bin"];
-        };
-      };
     };
 
-    podman = {
-      enable = true;
-      dockerCompat = true;
-    };
-
-    oci-containers.backend = "podman";
+    oci-containers.backend = "docker";
     oci-containers.containers = {
       pi-hole = {
         autoStart = true;
@@ -36,6 +26,7 @@
           "53:53/udp"
           "67:67/udp"
           "80:80/tcp"
+          "547:547/udp"
         ];
         environment = {
           TZ = "Europe/Brussels";
@@ -46,9 +37,9 @@
           PIHOLE_DOMAIN = "local";
           DNSMASQ_USER = "root";
           CUSTOM_CACHE_SIZE = "10000";
-          IPv6 = "false";
+          IPv6 = "true";
           DHCP_ACTIVE = "true";
-          DHCP_IPv6 = "false";
+          DHCP_IPv6 = "true";
           DHCP_START = "192.168.2.100";
           DHCP_END = "192.168.2.200";
           DHCP_ROUTER = "192.168.2.10";
@@ -59,16 +50,23 @@
           TEMPERATUREUNIT = "c";
           WEBUIBOXEDLAYOUT = "traditional";
           QUERY_LOGGING = "false";
-          FTLCONF_REPLY_ADDR4 = "192.168.2.10";
+          FTLCONF_LOCAL_ADDR4 = "192.168.2.10";
           WEBTHEME = "default-dark";
           PIHOLE_DNS_1 = "8.8.8.8";
           PIHOLE_DNS_2 = "8.8.4.4";
+          PIHOLE_DNS_3 = "2001:4860:4860:0:0:0:0:8888";
+          PIHOLE_DNS_4 = "2001:4860:4860:0:0:0:0:8844";
           PIHOLE_DNS_5 = " 208.67.222.222";
           PIHOLE_DNS_6 = "208.67.220.220";
+          PIHOLE_DNS_7 = "2620:119:35::35";
+          PIHOLE_DNS_8 = "2620:119:53::53";
           PIHOLE_DNS_9 = "1.1.1.1";
           PIHOLE_DNS_10 = "1.0.0.1";
+          PIHOLE_DNS_11 = "2606:4700:4700::1111";
+          PIHOLE_DNS_12 = "2606:4700:4700::1001";
           BLOCKING_ENABLED = "true";
           IPV4_ADDRESS = "0.0.0.0";
+          IPV6_ADDRESS = "0:0:0:0:0:0";
         };
         volumes = [
           "pihole:/etc/pihole"
@@ -78,7 +76,6 @@
           "--cap-add=NET_ADMIN"
           "--network=host"
           "--pull=newer"
-          "--dns=127.0.0.1"
         ];
       };
     };
