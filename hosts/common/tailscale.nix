656410db7b09c1c6eccdc906e3525f3096db9218
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-08 16:29:49 +0100

    update tailscale config

--- hosts/common/tailscale.nix
+++ hosts/common/tailscale.nix
@@ -41,7 +41,7 @@
     enable = true;
 
     # always allow traffic from your Tailscale network
-    trustedInterfaces = ["tailscale0"];
+    trustedInterfaces = [ config.services.tailscale.interfaceName ];
 
     # allow the Tailscale UDP port through the firewall
     allowedUDPPorts = [config.services.tailscale.port];
