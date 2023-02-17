52d16815f573784004f81404003cd797be454969
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-02-17 12:39:26 +0100

    fix: add `enp1s0u1` and `enp1s0u2` in case of interface name change

--- hosts/raspberryPi400/configuration.nix
+++ hosts/raspberryPi400/configuration.nix
@@ -106,12 +106,18 @@
     nat = {
       enable = true;
       internalInterfaces = ["end0"];
-      externalInterface = "enp1s0u2";
+      externalInterface = "enp1s0u1";
     };
 
     interfaces.end0.ipv4.addresses = [
       {
         address = "192.168.2.10";
+        prefixLength = 24;
+      }
+    ];
+    interfaces.enp1s0u1.ipv4.addresses = [
+      {
+        address = "192.168.1.2";
         prefixLength = 24;
       }
     ];
