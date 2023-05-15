b9c443094e4da5286ec501153ab78d706fce9aa9
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-05-15 09:09:40 +0200

    common: update fonts

--- hosts/common/packages.nix
+++ hosts/common/packages.nix
@@ -11,17 +11,12 @@
     dina-font
     du-dust
     fd
-    fira-code
-    fira-code-symbols
     fish
     gnupg
-    hack-font
     iosevka
     iosevka-comfy.comfy
-    jetbrains-mono
-    liberation_ttf
     neofetch
-    (nerdfonts.override {fonts = ["FiraCode" "DroidSansMono" "Hack" "JetBrainsMono"];})
+    nerdfonts
     p7zip
     procs
     ripgrep
