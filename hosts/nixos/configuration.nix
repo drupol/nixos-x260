48d7fa1ace007a3af1758d7135faa8d7d7e529e4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-05-16 18:13:52 +0200

    nixos: disable firewall

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -128,12 +128,15 @@
 
   programs.ssh.forwardX11 = true;
 
-  networking.firewall.allowedTCPPorts = [80 443 3389];
+  # networking.firewall.allowedTCPPorts = [
+  #   80
+  #   443
+  # ];
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
