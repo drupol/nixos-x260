3f8f2953e3026c210a2cc217ee5aac58fba14c51
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-05 16:31:08 +0200

    raspberrypi: caddy cleanup

--- hosts/raspberryPi400/caddy.nix
+++ hosts/raspberryPi400/caddy.nix
@@ -13,32 +13,16 @@
     enable = true;
 
     globalConfig = ''
-      debug
       auto_https off
     '';
 
     virtualHosts."raspberrypi400:80".extraConfig = ''
       handle_path /pihole/* {
-        rewrite * /admin{uri}
+        rewrite * /admin{path}
         reverse_proxy 127.0.0.1:8093
       }
       handle_path /grafana/* {
         reverse_proxy 127.0.0.1:8888
-      }
-      handle_path /wetty/* {
-        rewrite * /wetty{uri}
-        reverse_proxy 127.0.0.1:3000
-      }
-      handle_path /wetty* {
-        rewrite * /wetty{uri}
-        reverse_proxy 127.0.0.1:3000
-      }
-    '';
-
-    virtualHosts."wpad.lan:80".extraConfig = ''
-      handle * {
-        root * ${./resources/caddy}
-        file_server
       }
     '';
   };
