6389dddb2554b4410b29ade7e9f8e14440ec350e
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-05 18:31:59 +0200

    raspberrypi400: update

--- hosts/raspberryPi400/caddy.nix
+++ hosts/raspberryPi400/caddy.nix
@@ -11,15 +11,5 @@
 
   services.caddy = {
     enable = true;
-
-    virtualHosts."raspberrypi400:80".extraConfig = ''
-      handle_path /pihole/* {
-        rewrite * /admin{path}
-        reverse_proxy 127.0.0.1:8093
-      }
-      handle_path /grafana/* {
-        reverse_proxy 127.0.0.1:8888
-      }
-    '';
   };
 }
