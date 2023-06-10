2de49f38efcbd5ae95885386e90a825e930a1b85
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-06-10 12:09:15 +0200

    nixos: test openvscode-server-insiders

--- hosts/nixos/code.nix
+++ hosts/nixos/code.nix
@@ -1,14 +1,28 @@
-{ pkgs, ... }: {
+{ pkgs, callpackage, ... }: {
   services = {
     caddy.virtualHosts."code.nixos.lan".extraConfig = ''
       tls internal
       reverse_proxy http://127.0.0.1:3000
+    '';
+    caddy.virtualHosts."openvscode.nixos.lan".extraConfig = ''
+      tls internal
+      reverse_proxy http://127.0.0.1:4000
     '';
     code-server = {
       package = pkgs.master.code-server;
       enable = true;
       host = "127.0.0.1";
       port = 3000;
+      user = "pol";
+      auth = "none";
+      userDataDir = "/home/pol/.config/Code/";
+      extensionsDir = "/home/pol/.vscode/extensions/";
+    };
+    openvscode-server = {
+      package = callpackage ./openvscode-server-insiders.nix {};
+      enable = true;
+      host = "127.0.0.1";
+      port = 4000;
       user = "pol";
       auth = "none";
       userDataDir = "/home/pol/.config/Code/";
