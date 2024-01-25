9a569c75135f719b0a4830c5dd58adf292af9159
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-01-25 22:52:35 +0100

    router: update caddy config

--- hosts/router/caddy.nix
+++ hosts/router/caddy.nix
@@ -11,9 +11,10 @@
   services.caddy = {
     enable = true;
 
+    # From https://www.reddit.com/r/pihole/comments/xf7059/pihole_behind_caddy_reverse_proxy_redirecting_to/
     virtualHosts."router.lan:80".extraConfig = ''
       handle_path /pihole/* {
-        rewrite * /admin{path}
+        redir / /admin{path}
         reverse_proxy 127.0.0.1:8093
       }
       handle_path /grafana/* {
