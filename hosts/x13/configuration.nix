a8872fec61536f762241541b43285dd36c6c83cf
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-03-23 10:21:26 +0100

    x13: update kernel

--- hosts/x13/configuration.nix
+++ hosts/x13/configuration.nix
@@ -7,7 +7,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_7;
+  # boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_7;
 
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
