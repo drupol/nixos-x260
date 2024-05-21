e1fed100e6397886993447cf97b222967ffdb7c7
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-21 08:24:32 +0200

    messaging: add `makeChromiumApp` and `teams`

--- modules/home/messaging/default.nix
+++ modules/home/messaging/default.nix
@@ -15,11 +15,34 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      discord
-      element-desktop
-      signal-desktop
-      tdesktop
-    ];
+    home.packages =
+      let
+        # Proudly stolen from https://codeberg.org/xlambein/nixos
+        makeChromiumApp =
+          {
+            name,
+            desktopName,
+            url,
+            mimeTypes ? [ ],
+          }:
+          pkgs.makeDesktopItem {
+            inherit name desktopName mimeTypes;
+            exec = "${pkgs.chromium}/bin/chromium --enable-features=UseOzonePlatform --ozone-platform=wayland --app=${url}";
+            terminal = false;
+          };
+      in
+      with pkgs;
+      [
+        discord
+        element-desktop
+        signal-desktop
+        tdesktop
+        (makeChromiumApp {
+          name = "teams";
+          desktopName = "Teams";
+          url = "https://teams.microsoft.com/";
+          mimeTypes = [ "x-scheme-handler/msteams" ];
+        })
+      ];
   };
 }
