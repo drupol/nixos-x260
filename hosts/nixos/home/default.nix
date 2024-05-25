72bb4e8da235501903b9a0aefefb29b8c734706c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-05-25 10:53:29 +0200

    nixos: cleanup

--- hosts/nixos/home/default.nix
+++ hosts/nixos/home/default.nix
@@ -1,10 +1,10 @@
 { ... }:
 {
   thunderbird.enable = true;
-  firefox.enable = true;
-  code.enable = true;
+  # firefox.enable = true;
+  # code.enable = true;
   git.enable = true;
   shell.enable = true;
   desktop.enable = true;
-  messaging.enable = true;
+  # messaging.enable = true;
 }
