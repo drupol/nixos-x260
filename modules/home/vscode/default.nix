69bb05e6c8eb9b81beeaa7380812c8667e67e209
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-06 19:20:21 +0200

    vscode: fix deprecation

--- modules/home/vscode/default.nix
+++ modules/home/vscode/default.nix
@@ -43,7 +43,6 @@ in
         pkgs.vscode-extensions.jebbs.plantuml
         pkgs.vscode-extensions.jkillian.custom-local-formatters
         pkgs.vscode-extensions.jnoortheen.nix-ide
-        pkgs.vscode-extensions.matklad.rust-analyzer
         pkgs.vscode-extensions.mgt19937.typst-preview
         pkgs.vscode-extensions.mhutchie.git-graph
         pkgs.vscode-extensions.mkhl.direnv
