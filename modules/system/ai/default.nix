e273499e9ebdf0afc8ebc91fdb9f438ebbc3b01a
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-07 14:36:46 +0200

    ai: update module config

--- modules/system/ai/default.nix
+++ modules/system/ai/default.nix
@@ -15,24 +15,24 @@ in
     services = {
       open-webui = {
         enable = true;
+        openFirewall = true;
+        host = "0.0.0.0";
         environment = {
-          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+          ENABLE_OLLAMA_API = "True";
+          OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
           HF_HOME = "/var/lib/open-webui/";
           SENTENCE_TRANSFORMERS_HOME = "/var/lib/open-webui/";
           WEBUI_AUTH = "False";
+          DEVICE_TYPE = "cpu";
+          ENABLE_RAG_HYBRID_SEARCH = "True";
+          RAG_EMBEDDING_ENGINE = "ollama";
+          RAG_EMBEDDING_MODEL = "mxbai-embed-large:latest";
         };
       };
 
       ollama = {
         enable = true;
-      };
-    };
-
-    networking = {
-      firewall = {
-        allowedTCPPorts = [
-          8080
-        ];
       };
     };
   };
