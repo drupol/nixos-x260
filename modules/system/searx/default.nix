53707ac4bd877026d66061ad5a9c35c3caf5c8dc
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-18 21:24:16 +0200

    searx: add base_url

--- modules/system/searx/default.nix
+++ modules/system/searx/default.nix
@@ -23,6 +23,7 @@ in
             bind_address = "127.0.0.1";
             secret_key = "spotting-gumminess-chamomile-unsuited-purple";
             image_proxy = true;
+            base_url = "/searx";
           };
         };
       };
