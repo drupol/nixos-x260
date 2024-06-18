d2458361c5130348e234683a419295356dfd3edf
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-18 21:44:16 +0200

    searx: update

--- modules/system/searx/default.nix
+++ modules/system/searx/default.nix
@@ -18,12 +18,27 @@ in
       searx = {
         enable = true;
         settings = {
+          use_default_settings = true;
+
+          general = {
+            privacypolicy_url = false;
+            enable_metrics = true;
+            debug = false;
+          };
+
           server = {
             port = 3002;
             bind_address = "127.0.0.1";
             secret_key = "spotting-gumminess-chamomile-unsuited-purple";
             image_proxy = true;
             base_url = "/searx";
+          };
+
+          search = {
+            safe_search = 0; # 0 = None, 1 = Moderate, 2 = Strict
+            formats = ["html" "json" "rss"];
+            autocomplete = "google"; # "dbpedia", "duckduckgo", "google", "startpage", "swisscows", "qwant", "wikipedia" - leave blank to turn it off by default
+            default_lang = "en";
           };
         };
       };
