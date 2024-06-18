1db586e8b504604afb839386061f0437fc272dc6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-06-18 21:27:38 +0200

    ai: remove `handle_path`

--- modules/system/ai/default.nix
+++ modules/system/ai/default.nix
@@ -44,9 +44,7 @@ in
       caddy = {
         enable = true;
         virtualHosts."192.168.1.37".extraConfig = ''
-          handle_path /ai/* {
-            reverse_proxy 127.0.0.1:8080
-          }
+          reverse_proxy 127.0.0.1:8080
         '';
       };
     };
