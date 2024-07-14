bd21cae8a947e4b4c592a45e40d5fbde368e78d4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-07-14 21:11:56 +0200

    ai: update config

--- modules/system/ai/default.nix
+++ modules/system/ai/default.nix
@@ -31,25 +31,24 @@ in
         package = pkgs.master.open-webui;
         port = 8080;
         environment = {
-          WEBUI_AUTH = "False";
+          CONTENT_EXTRACTION_ENGINE = "tika";
+          DEVICE_TYPE = "cpu";
           ENABLE_OLLAMA_API = "True";
+          ENABLE_RAG_HYBRID_SEARCH = "True";
+          ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
+          ENABLE_RAG_WEB_SEARCH = "True";
           OLLAMA_BASE_URL = "http://127.0.0.1:11434";
           OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
-          DEVICE_TYPE = "cpu";
-          ENABLE_RAG_HYBRID_SEARCH = "True";
-          RESET_CONFIG_ON_START = "True";
           RAG_EMBEDDING_ENGINE = "ollama";
           RAG_EMBEDDING_MODEL = "mxbai-embed-large:latest";
           RAG_EMBEDDING_MODEL_AUTO_UPDATE = "True";
           RAG_RERANKING_MODEL_AUTO_UPDATE = "True";
-          RAG_RERANKING_MODEL = "mxbai-embed-large:latest";
-          ENABLE_RAG_WEB_SEARCH = "True";
-          ENABLE_RAG_WEB_LOADER_SSL_VERIFICATION = "False";
           RAG_WEB_SEARCH_ENGINE = "searxng";
-          SEARXNG_QUERY_URL = "http://127.0.0.1/searx/search?q=<query>";
           RAG_WEB_SEARCH_RESULT_COUNT = "5";
-          CONTENT_EXTRACTION_ENGINE = "tika";
+          RESET_CONFIG_ON_START = "True";
+          SEARXNG_QUERY_URL = "http://127.0.0.1/searx/search?q=<query>";
           TIKA_SERVER_URL = "http://127.0.0.1:9998/";
+          WEBUI_AUTH = "False";
         };
       };
 
