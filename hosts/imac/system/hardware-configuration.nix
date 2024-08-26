c7072d8f3b1653aa7d8d047c5b6a96aee08fc87d
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-08-26 12:42:07 +0200

    imac: update config

--- hosts/imac/system/hardware-configuration.nix
+++ hosts/imac/system/hardware-configuration.nix
@@ -39,4 +39,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.bluetooth.enable = true;
 }
