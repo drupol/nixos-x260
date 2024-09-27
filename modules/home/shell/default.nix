144b532fc4cad447cb336a0fc8011b51abbbe0d3
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-09-27 10:21:21 +0200

    module/shell: configure starship for jujutsu

--- modules/home/shell/default.nix
+++ modules/home/shell/default.nix
@@ -446,30 +446,35 @@ in
 
           custom.jj = {
             when = "jj root";
+            format = " $output";
             command = ''
-              jj log -r@ -l1 --ignore-working-copy --no-graph --color always  -T '
-                separate(" ",
-                  branches.map(|x| if(
-                      x.name().substr(0, 10).starts_with(x.name()),
-                      x.name().substr(0, 10),
-                      x.name().substr(0, 9) ++ "…")
-                    ).join(" "),
-                  tags.map(|x| if(
-                      x.name().substr(0, 10).starts_with(x.name()),
-                      x.name().substr(0, 10),
-                      x.name().substr(0, 9) ++ "…")
-                    ).join(" "),
-                  surround("\"","\"",
-                    if(
-                       description.first_line().substr(0, 24).starts_with(description.first_line()),
-                       description.first_line().substr(0, 24),
-                       description.first_line().substr(0, 23) ++ "…"
-                    )
-                  ),
-                  if(conflict, "conflict"),
-                  if(divergent, "divergent"),
-                  if(hidden, "hidden"),
-                )
+              jj log -n 1 --ignore-working-copy --no-graph --color always -r @ -T '
+                  surround(
+                      "(",
+                      ")",
+                      separate(
+                          " ",
+                          branches.join(", "),
+                          coalesce(
+                              surround(
+                                  "\"",
+                                  "\"",
+                                  if(
+                                      description.first_line().substr(0, 24).starts_with(description.first_line()),
+                                      description.first_line().substr(0, 24),
+                                      description.first_line().substr(0, 23) ++ "…"
+                                  )
+                              ),
+                              "(no description set)"
+                          ),
+                          change_id.shortest(),
+                          commit_id.shortest(),
+                          if(conflict, "(conflict)"),
+                          if(empty, "(empty)"),
+                          if(divergent, "(divergent)"),
+                          if(hidden, "(hidden)"),
+                      )
+                  )
               '
             '';
             detect_folders = [ ".jj" ];
