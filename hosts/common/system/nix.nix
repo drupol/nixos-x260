ec57c0f1eb32c19e12fb4c347e0e64343b65f519
Pol Dellaiera <pol.dellaiera@protonmail.com>
2024-12-23 12:03:26 +0100

    common: update nix config

--- hosts/common/system/nix.nix
+++ hosts/common/system/nix.nix
@@ -17,20 +17,21 @@
       min-free = 128000000
       max-free = 1000000000
       fallback = true
-      warn-dirty = false
-      experimental-features = nix-command flakes
     '';
     gc = {
       automatic = true;
       dates = "daily";
       options = "--delete-older-than 2d";
     };
+    optimise.automatic = true;
     settings = {
       trusted-users = [
         "root"
         "pol"
       ];
       auto-optimise-store = true;
+      experimental-features = [ "nix-command" "flakes" ];
+      warn-dirty = false;
     };
   };
 
