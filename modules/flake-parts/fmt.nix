cdd8ef3cd6e720cfe4cefdb9f0017462b46a8cec
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-06-02 21:33:47 +0200

    refactor: update `fmt` module

--- modules/flake-parts/fmt.nix
+++ modules/flake-parts/fmt.nix
@@ -1,37 +1,39 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem = {
-    treefmt = {
-      projectRootFile = "flake.nix";
-      programs = {
-        nixfmt.enable = true;
-        prettier.enable = true;
-        rustfmt.enable = true;
-        shfmt.enable = true;
-        yamlfmt.enable = true;
+  perSystem =
+    { self', ... }:
+    {
+      treefmt = {
+        projectRootFile = "flake.nix";
+        programs = {
+          nixfmt.enable = true;
+          prettier.enable = true;
+          rustfmt.enable = true;
+          shfmt.enable = true;
+          yamlfmt.enable = true;
+        };
+        settings = {
+          on-unmatched = "fatal";
+          global.excludes = [
+            "*.envrc"
+            ".editorconfig"
+            "*.directory"
+            "*.face"
+            "*.fish"
+            "*.png"
+            "*.toml"
+            "*.svg"
+            "*.xml"
+            "*/.gitignore"
+            "_to_migrate/*"
+            "LICENSE"
+          ];
+        };
       };
-      settings = {
-        on-unmatched = "fatal";
-        global.excludes = [
-          "*.envrc"
-          ".editorconfig"
-          "*.directory"
-          "*.face"
-          "*.fish"
-          "*.png"
-          "*.toml"
-          "*.svg"
-          "*.xml"
-          "*/.gitignore"
-          "_to_migrate/*"
-          "LICENSE"
-        ];
+      pre-commit.settings.hooks.nix-fmt = {
+        enable = true;
+        entry = lib.getExe self'.formatter;
       };
     };
-    pre-commit.settings.hooks.nix-fmt = {
-      enable = true;
-      entry = "nix fmt -- --fail-on-change";
-    };
-  };
 }
