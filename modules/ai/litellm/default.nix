a896f845bac5932cf9b21052b20c5546fb3f7872
Pol Dellaiera <pol.dellaiera@protonmail.com>
2025-06-02 20:49:27 +0200

    ai/litellm: refactor

--- modules/ai/litellm/default.nix
+++ modules/ai/litellm/default.nix
@@ -1,83 +1,66 @@
 {
-  inputs,
-  ...
-}:
-{
-  flake.modules = {
-    nixos.ai =
-      { pkgs, ... }:
-      {
-        nixpkgs.overlays = [
-          (final: prev: {
-            master = import inputs.nixpkgs-master {
-              inherit (final) config system;
-            };
-          })
-        ];
-
-        services = {
-          litellm = {
-            enable = true;
-            package = pkgs.litellm;
-            host = "0.0.0.0";
-            port = 8888;
-            settings = {
-              model_list = [
-                {
-                  model_name = "*";
-                  litellm_params = {
-                    model = "openai/*";
-                    api_key = "os.environ/OPENAI_API_KEY";
-                  };
-                }
-                {
-                  model_name = "gpt-4o";
-                  litellm_params = {
-                    model = "openai/gpt-4o";
-                    api_key = "os.environ/OPENAI_API_KEY";
-                  };
-                }
-                {
-                  model_name = "deepseek-v3";
-                  litellm_params = {
-                    model = "github/deepseek-v3";
-                    api_key = "os.environ/GITHUB_API_KEY";
-                  };
-                }
-                {
-                  model_name = "deepseek-r1";
-                  litellm_params = {
-                    model = "github/deepseek-r1";
-                    api_key = "os.environ/GITHUB_API_KEY";
-                  };
-                }
-                {
-                  model_name = "Llama-3.3-70B-Instruct";
-                  litellm_params = {
-                    model = "github/Llama-3.3-70B-Instruct";
-                    api_key = "os.environ/GITHUB_API_KEY";
-                  };
-                }
-                {
-                  model_name = "Gemini";
-                  litellm_params = {
-                    model = "gemini/gemini-2.5-pro-exp-03-25";
-                    api_key = "os.environ/GEMINI_API_KEY";
-                  };
-                }
-                {
-                  model_name = "mistral/open-mistral-nemo";
-                  litellm_params = {
-                    model = "mistral/open-mistral-nemo";
-                    api_key = "os.environ/MISTRAL_API_KEY";
-                  };
-                }
-              ];
-            };
-            environmentFile = "/home/pol/Code/drupol/litellm-secrets.env";
-            openFirewall = true;
-          };
+  flake.modules.nixos.ai = {
+    services = {
+      litellm = {
+        enable = true;
+        host = "0.0.0.0";
+        port = 8888;
+        settings = {
+          model_list = [
+            {
+              model_name = "*";
+              litellm_params = {
+                model = "openai/*";
+                api_key = "os.environ/OPENAI_API_KEY";
+              };
+            }
+            {
+              model_name = "gpt-4o";
+              litellm_params = {
+                model = "openai/gpt-4o";
+                api_key = "os.environ/OPENAI_API_KEY";
+              };
+            }
+            {
+              model_name = "deepseek-v3";
+              litellm_params = {
+                model = "github/deepseek-v3";
+                api_key = "os.environ/GITHUB_API_KEY";
+              };
+            }
+            {
+              model_name = "deepseek-r1";
+              litellm_params = {
+                model = "github/deepseek-r1";
+                api_key = "os.environ/GITHUB_API_KEY";
+              };
+            }
+            {
+              model_name = "Llama-3.3-70B-Instruct";
+              litellm_params = {
+                model = "github/Llama-3.3-70B-Instruct";
+                api_key = "os.environ/GITHUB_API_KEY";
+              };
+            }
+            {
+              model_name = "Gemini";
+              litellm_params = {
+                model = "gemini/gemini-2.5-pro-exp-03-25";
+                api_key = "os.environ/GEMINI_API_KEY";
+              };
+            }
+            {
+              model_name = "mistral/open-mistral-nemo";
+              litellm_params = {
+                model = "mistral/open-mistral-nemo";
+                api_key = "os.environ/MISTRAL_API_KEY";
+              };
+            }
+          ];
         };
+        environmentFile = "/home/pol/Code/drupol/litellm-secrets.env";
+        openFirewall = true;
       };
+    };
   };
 }
